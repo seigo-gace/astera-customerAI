878122924a59bd2a8c98f00f6a04492f7bad9825
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-10 07:37:50 +0900

    feat: enable Notion KB v3 adapter

--- runtime/__init__.py
+++ runtime/__init__.py
@@ -2,6 +2,10 @@ from .bundled_snapshot import install_bundled_notion_fallback
 
 install_bundled_notion_fallback()
 
+from .notion_v3 import install_v3_notion_adapter
+
+install_v3_notion_adapter()
+
 from .service import CustomerAIService
 
 __all__ = ["CustomerAIService"]
